4997c35d0bb2f6f585b074fa0318b0a8d31ea859
kakilangit <kakilangit@gmail.com>
2026-05-18 09:36:06 +0200

    Add swarms: swarm, members, coordinator, runner, turn router

---
+++ lib/summoner/swarms/swarm_member.ex
@@ -0,0 +1,36 @@
+defmodule Summoner.Swarms.SwarmMember do
+  @moduledoc """
+  Join schema linking agents to swarms with ordering.
+  """
+
+  use Summoner.Schema
+
+  import Ecto.Changeset
+
+  alias Summoner.Agents.Agent
+  alias Summoner.Swarms.Swarm
+
+  schema "swarm_members" do
+    field :position, :integer, default: 0
+
+    belongs_to :swarm, Swarm
+    belongs_to :agent, Agent
+
+    timestamps(updated_at: false)
+  end
+
+  @required_fields ~w(swarm_id agent_id)a
+  @optional_fields ~w(position)a
+
+  def changeset(member, attrs) do
+    member
+    |> cast(attrs, @required_fields ++ @optional_fields)
+    |> validate_required(@required_fields)
+    |> validate_number(:position, greater_than_or_equal_to: 0)
+    |> unique_constraint([:swarm_id, :agent_id],
+      message: "familiar already in this coven"
+    )
+    |> foreign_key_constraint(:swarm_id)
+    |> foreign_key_constraint(:agent_id)
+  end
+end
